4bdda379a806138b7ed3899516c288c7592943bd
gloriatampuri <gloria.tampuri@amalitech.org>
2023-07-14 13:31:04 +0000

    drag and drop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wholePage = document.querySelectorAll(".wholePage");
 const heroBackground = document.getElementById("hero-bg");
-const switchIcon = document.getElementById("icon");
+const darkModeToggle  = document.getElementById("icon");
 const listContainer = document.getElementById('list-container')
 const inputBox = document.getElementById("input")
 const addCircle=document.getElementById("add")
@@ -10,89 +10,43 @@ const clearTasks=document.getElementById('clearTask')
 const selectedItems =document.getElementsByClassName('checked')
 const mobileSort = document.getElementById('mobileSort')
 const mediaQuery =window.matchMedia("(max-width:400px)")
+const all= document.getElementById('all')
+const active= document.getElementById('active')
+const completed=document.getElementById('completed')
 
 
+darkModeToggle.addEventListener('click', function() {
+  const darkModeEnabled = document.body.classList.toggle('dark-switch');
+ 
+  localStorage.setItem('darkMode', darkModeEnabled);
 
-// document.addEventListener("DOMContentLoaded",function(){
-//     function enableDarkMode(){
-//     document.body.classList.add('dark-switch')
-//     localStorage.setItem('mode','dark')      
+  // Change the src attribute based on the dark mode state
+  if (darkModeEnabled) {
+    darkModeToggle.src='images/icon-sun.svg';
+    heroBackground.classList.remove('hero-image')
+    heroBackground.classList.add('dark-hero')
+  } else {
+    heroBackground.classList.remove('dark-hero')
+    heroBackground.classList.add('hero-image')
+    darkModeToggle.src = 'images/icon-moon.svg';
+  }
+});
+
+window.addEventListener('load', function() {
+    const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
     
+    if (darkModeEnabled) {
+      document.body.classList.add('dark-switch');
+    darkModeToggle.src='images/icon-sun.svg';
+    heroBackground.classList.add('dark-hero')
 
-// }
-// function disableDarkMode(){
-//      document.body.classList.remove('dark-switch')
-//     localStorage.setItem('mode','light')
-    
-
-// }
-// let preferredmode = localStorage.getItem('mode')
-
-// const saveMode=()=>{
-//     localStorage.setItem("mode", preferredmode )
-// }
-
-// switchIcon.onclick=function(){
-//    const isDarkMode = localStorage.getItem('mode')==='dark';
-//    if(isDarkMode){
-//     disableDarkMode()
-//     saveMode()
-
-//    }else{
-//     enableDarkMode()
-//     saveMode()
-//    }
-
-//     if(preferredmode==='dark'){
-//         icon.src='/images/icon-sun.svg';
-//         heroBackground.classList.remove("hero-image")
-//         heroBackground.classList.add("dark-hero")
-//         console.log('dark');
-//         enableDarkMode()
-//         saveMode()
-
-//     }
-//     else if(preferredmode==='light'){
-//         icon.src='/images/icon-moon.svg';
-//         heroBackground.classList.remove("dark-hero")
-//         heroBackground.classList.add("hero-image")
-//         disableDarkMode()
-//         saveMode()
-//     }    
-   
-// }
-// })
-
-
-
-switchIcon.onclick=function(){
-        const setTheme= document.body;
-        setTheme.classList.toggle('dark-switch')
-    
-        let theme;
-    
-        if(setTheme.classList.contains('dark-switch')){
-            console.log('dark mode');
-            theme ='DARK'
-        }else{
-            console.log('Light mode');
-            theme='LIGHT'
-        }
-    
-        localStorage.setItem('PageTheme', JSON.stringify(theme))
-    
-    
-    let getTheme = JSON.parse(localStorage.getItem('PageTheme'));
-    console.log(getTheme);
-    
-    if(getTheme==='DARK'){
-        document.body.classList='dark-switch'
     }
-}
+  });
+  
 
 
 
-
+//Add Task
 addCircle.onclick=function(){
     if(inputBox.value ===''){
         alert('Please type a task!')
@@ -101,14 +55,16 @@ addCircle.onclick=function(){
     
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
+        li.draggable=true;
         let span=document.createElement("span");
        let img =document.createElement('img');
        img.src='images/icon-cross.svg'
        span.appendChild(img)
         li.appendChild(span)
-      
-        // location.reload();    
+        li.classList.add=('draggable')
+        location.reload();
+
     }
     inputBox.value=''
     saveData()
@@ -116,7 +72,7 @@ addCircle.onclick=function(){
 }
 
 
-
+//check completed task and removed task my clicking class icon
 listContainer.onclick=function(e){
     if(e.target.tagName==="LI"){
         e.target.classList.toggle('checked')
@@ -125,7 +81,7 @@ listContainer.onclick=function(e){
     else if(e.target.tagName=== 'IMG'){
         e.target.parentElement.parentElement.remove();
         saveData()
-        // location.reload()
+        location.reload()
     }
 }
 
@@ -141,32 +97,123 @@ function showData(){
 }
 showData()
 console.log(listContainer.childElementCount);
+
+//show summary and sort
 if(listContainer.childElementCount>=1){
   filterDiv.style.display="flex"
 }
 if (mediaQuery.matches && listContainer.childElementCount>=1){
     mobileSort.classList.add('.sortOnMobile')
-    mobileSort.style.display='flex' 
+    // mobileSort.style.display='flex' 
 }
 
-showNumberOftask=()=>{
+//show number of task in list
+const showNumberOftask=()=>{
     let number = listContainer.childElementCount
     console.log(number);
-    const numberSpan = document.createElement('span')
-    numberSpan.textContent = number
     const item = number===1?'item':'items'
-    taskCount.appendChild()
+
+    taskCount.textContent=(`${number} ${item} left`)
 }
 showNumberOftask()
 
-
+//clear completed task
 clearTasks.onclick=function(){
-    console.log(selectedItems);
+    
 const completedArray = Array.from(selectedItems)
-console.log(completedArray);
+
 completedArray.forEach(function(selectedItem){
     selectedItem.parentNode.removeChild(selectedItem)
 })
     saveData()
-    // location.reload()
+    location.reload()
+}
+
+//filter task
+all.addEventListener('click', function() {
+    filterList('all');
+  });
+  
+  completed.addEventListener('click', function() {
+    filterList('completed');
+  });
+  
+  active.addEventListener('click', function() {
+    filterList('active');
+  });
+  
+  function filterList(filter) {
+    const listItems = listContainer.children;
+  
+    for (let i = 0; i < listItems.length; i++) {
+      const listItem = listItems[i];
+  
+      switch (filter) {
+        case 'all':
+          listItem.style.display = 'block';
+          break;
+        case 'completed':
+          if (listItem.classList.contains('checked')) {
+            listItem.style.display = 'block';
+          } else {
+            listItem.style.display = 'none';
+          }
+          break;
+        case 'active':
+          if (!listItem.classList.contains('checked')) {
+            listItem.style.display = 'block';
+          } else {
+            listItem.style.display = 'none';
+          }
+          break;
+      }
+    }
+  }
+  
+//Drag and drop
+
+const draggables=document.getElementsByTagName('li')
+const draggablesArray = Array.from(draggables);
+
+console.log(draggablesArray);
+draggablesArray.forEach(draggable=>{
+draggable.draggable=true;
+})
+draggablesArray.forEach(draggable=>{
+  draggable.addEventListener('dragstart', ()=>{
+  console.log('dragstart');
+    draggable.classList.add('dragging')
+  })
+  draggable.addEventListener('dragend',()=>{
+    draggable.classList.remove('dragging')
+  })
+})
+
+// 
+
+listContainer.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  const draggable = document.querySelector('.dragging');
+  const afterElement = getDragAfterElement(listContainer, e.clientY);
+
+  if (afterElement && listContainer.contains(afterElement)) {
+    listContainer.insertBefore(draggable, afterElement);
+  } else {
+    listContainer.appendChild(draggable);
+  }
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll(':not(.dragging)')];
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child };
+    } else {
+      return closest;
+    }
+  }, { offset: Number.NEGATIVE_INFINITY, element: null }).element;
 }
